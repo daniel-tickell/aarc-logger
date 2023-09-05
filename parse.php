90c5e287feac0c295e7fa438d6b10e7b12ba802e
daniel-tickell <86997827+daniel-tickell@users.noreply.github.com>
2023-09-05 11:15:27 -0700

    Update parse.php

--- parse.php
+++ parse.php
@@ -1,80 +1,83 @@
-<html>
-
-<table>
-
 <?php
-include 'adif_parser.php';
+include_once("config.php");
 
-$p = new ADIF_Parser;
+if(isset($_POST['Submit'])){ // Fetching variables of the form which travels in URL
+        $callsign = strtoupper($_POST['callsign']);
+        $grid = strtoupper($_POST['grid']);
+        $dst_callsign = strtoupper($_POST['dst_callsign']);
+        $dst_grid = strtoupper($_POST['dst_grid']);
 
-$p->feed("ADIF File as String");
+        $mode = $_POST['mode'];
+        $freq = $_POST['freq'];
+        $date = $_POST['date'];
+        $time = 'CURRENT_TIMESTAMP';
+        if (!$date){
+                $date = 'CURRENT_TIMESTAMP';
+        }
+}
 
-$p->load_from_file($fname);
+if ($freq >= 1.8 && $freq <= 2.0){
+        $band = '160m';
+}
+else if ($freq >= 3.5 && $freq < 4.0){
+        $band = '80m';
+}
+else if ($freq >= 5.3 && $freq <= 5.405){
+        $band = '60m';
+}
+else if ($freq >= 7 && $freq <= 7.3){
+        $band = '40m';
+}
+else if ($freq >= 10.1 && $freq <= 10.150){
+        $band = '30m';
+}
+else if ($freq >= 14.000 && $freq <= 14.350){
+        $band = '20m';
+}
+else if ($freq >= 18.068 && $freq <= 18.168){
+        $band = '17m';
+}
+else if ($freq >= 21 && $freq <= 21.450){
+        $band = '15m';
+}
+else if ($freq >= 24.890 && $freq <= 24.990){
+        $band = '12m';
+}
+else if ($freq >= 28 && $freq <= 29.7){
+        $band = '10m';
+}
+else if ($freq >= 50 && $freq <= 54){
+        $band = '6m';
+}
+else if ($freq >= 144 && $freq <= 148){
+        $band = '2m';
+}
+else if ($freq >= 219 && $freq <= 225){
+        $band = '1.25m';
+}
+else if ($freq >= 420 && $freq <= 450){
+        $band = '70cm';
+}
+else if ($freq >= 902 && $freq <= 928){
+        $band = '33cm';
+}
+else if ($freq >= 1240 && $freq <= 1300){
+        $band = '23cm';
+}
+else {
+        $band = "Out of Ham Band";
+}
 
-$p = new ADIF_Parser;
-$p->load_from_file("test.adi");
-$p->initialize();
 
-  echo '<table id="qsos">'.
-      '<tr>
-          <th>Time</th>
-          <th>My Callsign</th>
-          <th>My grid</th>
-          <th>Dest Callsign</th>
-          <th>Dest Grid</th>
-          <th>Band</th>
-          <th>Frequency</th>
-          <th>Mode</th>
-          <th>Date</th>
-        <tr>';
+$sql = "insert into log_test (src_callsign, grid, dst_callsign, dst_grid,  band, mode, freq, timestamp, date, time)
+VALUES ('$callsign', '$grid', '$dst_callsign', '$dst_grid', '$band', '$mode', '$freq', CURRENT_TIMESTAMP, '$date', CURRENT_TIMESTAMP)";
 
-while($row = $p->get_record())
-{
-	if(count($row) == 0)
-	{
-		break;
-	};
+if (!mysqli_query($mysqli, $sql)) {
+        die('Something Went Wrong inserting your record into the database' . $sql);
+} else {
+    echo "Record Inserted";
+}
 
-	$format_time = $row["time_on"][0].$row["time_on"][1].":".$row["time_on"][2].$row["time_on"][3].":".$row["time_on"][4].$row["time_on"][5];
-	$format_date = $row["qso_date"][0].$row["qso_date"][1].$row["qso_date"][2].$row["qso_date"][3]."-".$row["qso_date"][4].$row["qso_date"][5]."-".$row["qso_date"][6].$row["qso_date"][7];
-    echo "<tr><td> " . $format_time . 
+header('Location: index.php?call='. $callsign . "&grid=" . $grid . "&freq=" . $freq . "&mode=" . $mode);
 
-         "</td><td> " . $row["station_callsign"]. 
-         "</td><td> " . $row["my_gridsquare"].
-         "</td><td> " . $row["call"]. 
-         "</td><td> " . $row["gridsquare"].
-         "</td><td> " . $row["band"].
-         "</td><td> " . $row["freq"].
-         "</td><td> " . $row["mode"].
-         "<td> " . $format_date. 
-
-              "</td><tr>";
-
-};
 ?>
-
-<style>
-	#qsos {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 60%;
-  text-align: center;
-}
-
-#qsos td, #qsos th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#qsos tr:nth-child(even){background-color: #f2f2f2;}
-
-#qsos tr:hover {background-color: #ddd;}
-
-#qsos th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #04AA6D;
-  color: white;
-}
-</style>
